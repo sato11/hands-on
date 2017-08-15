3b14e765b467ddfe1fff1d2123257f9fbee49688
Sato Junichi <villanova911@gmail.com>
2017-08-15 14:05:44 +0900

    define default task to watch changes

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,6 +4,11 @@ var autoprefixer = require('gulp-autoprefixer');
 var frontnote = require('gulp-frontnote');
 var uglify = require('gulp-uglify');
 
+gulp.task('default', function() {
+    gulp.watch(['js/**/*.js','!js/min/**/*.js'],['js']);
+    gulp.watch('sass/**/*.scss',['sass']);
+});
+
 gulp.task('sass', function() {
     gulp.src('sass/**/*scss')
         .pipe(frontnote({

4fa42ab58088bffd42591373a9fa1b1ab5c2c085
Sato Junichi <villanova911@gmail.com>
2017-08-15 14:33:00 +0900

    not to cancel watching on error

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,6 +4,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var frontnote = require('gulp-frontnote');
 var uglify = require('gulp-uglify');
 var browser = require('browser-sync');
+var plumber = require('gulp-plumber');
 
 gulp.task('server', function() {
     browser({
@@ -15,12 +16,14 @@ gulp.task('server', function() {
 
 gulp.task('js', function() {
     gulp.src(['js/**/*.js', '!js/min/**/*.js'])
+        .pipe(plumber())
         .pipe(uglify())
         .pipe(gulp.dest('./js/min'));
 });
 
 gulp.task('sass', function() {
     gulp.src('sass/**/*scss')
+        .pipe(plumber())
         .pipe(frontnote({
             css: '../css/style.css'
         }))

f6a3b7b9f76538c25a703ae155c57d7e0f126196
Sato Junichi <villanova911@gmail.com>
2017-08-15 14:28:36 +0900

    import style guide

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var frontnote = require('gulp-frontnote');
 
 gulp.task('sass', function() {
     gulp.src('sass/**/*scss')
+        .pipe(frontnote({
+            css: '../css/style.css'
+        }))
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(gulp.dest('./css'));
